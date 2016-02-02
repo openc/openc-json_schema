fbb9178e31b87ed2b53323255bc5cde489278130
James McKinney <james@slashpoundbang.com>
2016-02-02 13:36:11 -0500

    No need to write temporary files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,27 +7,10 @@ end
 
 require 'openc/json_schema'
 
-def get_schema_path(schema_or_path)
-  case schema_or_path
-  when Hash
-    json_data = schema_or_path.to_json
-    filename = Digest::MD5.hexdigest(json_data) + '.json'
-    schema_path = File.join('spec', 'tmp', filename)
-    File.open(schema_path, 'w') {|f| f.write(json_data)}
-  when String
-    schema_path = schema_or_path
-  else
-    raise
-  end
-
-  schema_path
-end
-
 RSpec::Matchers.define(:fail_validation_with) do |expected|
-  match do |actual|
-    schema_or_path, record = actual
-    @error = Openc::JsonSchema.validate(get_schema_path(schema_or_path), record)[:message]
-    expect(@error).to eq(expected)
+  match do |(schema_or_path, record)|
+    error = Openc::JsonSchema.validate(schema_or_path, record)
+    expect(error[:message]).to eq(expected)
   end
 
   failure_message do |actual|
@@ -36,19 +19,15 @@ RSpec::Matchers.define(:fail_validation_with) do |expected|
 end
 
 RSpec::Matchers.define(:be_valid) do
-  match do |actual|
-    schema_or_path, record = actual
-    error = Openc::JsonSchema.validate(get_schema_path(schema_or_path), record)
+  match do |(schema_or_path, record)|
+    error = Openc::JsonSchema.validate(schema_or_path, record)
     expect(error).to eq(nil)
   end
 end
 
 RSpec::Matchers.define(:convert_dates_to) do |expected|
-  match do |actual|
-    schema_or_path, record = actual
-    schema_path = get_schema_path(schema_or_path)
-
-    converted_record = Openc::JsonSchema.convert_dates(schema_path, record)
+  match do |(schema_or_path, record)|
+    converted_record = Openc::JsonSchema.convert_dates(schema_or_path, record)
     expect(converted_record).to eq(expected)
   end
 end
