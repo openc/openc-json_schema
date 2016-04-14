d1f0b90eaa50d49c4da2371abd3c09c55c272396
seb <seb.bacon@gmail.com>
2016-04-14 10:56:16 +0100

    Improved expectation-failure output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,12 +9,17 @@ require 'openc/json_schema'
 
 RSpec::Matchers.define(:fail_validation_with) do |expected|
   match do |(schema_or_path, record)|
-    @actual = Openc::JsonSchema.validate(schema_or_path, record)[:message]
-    expect(@actual).to eq(expected)
+    @actual = Openc::JsonSchema.validate(schema_or_path, record)
+    expect(@actual).to_not be(nil)
+    expect(@actual[:message]).to eq(expected)
   end
 
   failure_message do |actual|
-    "Expected error to be #{expected}, but was #{@actual}"
+    if actual.nil?
+      "Expected error, but there was none"
+    else
+      "Expected error to be #{expected}, but was #{actual}"
+    end
   end
 end
 
